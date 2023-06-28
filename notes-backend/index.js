85b8223893008cd22d66d43d3feb692fe76646b4
Arobase <sergedelonboka92@gmail.com>
2023-06-28 08:36:41 +0000

    Add a route to fetch all ressources in the collection

---
+++ notes-backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+
+app.use(cors())
+
+
+let notes = [
+	{
+		id: 1,
+		content: "HTML is easy",
+		important: true
+	},
+	{
+		id: 2,
+		content: "Browser can execute only JavaScript",
+		important: false
+	},
+	{
+		id: 3,
+		content: "GET and POST are the most important methods of HTTP protocol",
+		important: true
+	}
+]
+
+app.get('/', (req, res) =>{
+	res.send(`<h1>Hello world</h1>`)
+})
+
+app.get('/api/notes', (req, res) => {
+	res.json(notes);
+})
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port: ${PORT}`)
+})
